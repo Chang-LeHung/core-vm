a3c7d3558858cfd7a7b8e3d0ee707d5b67817103
changlehung <changlehung@gmail.com>
2024-12-21 00:10:26 +0800

    fix: fix interpreter bugs

--- src/bytecodeinterpreter.cpp
+++ src/bytecodeinterpreter.cpp
@@ -4,23 +4,24 @@
 #include "opcode.h"
 #include "types.h"
 #include "util.h"
+#include <iostream>
 
 #define TOP_AS_TYPE(type) (*(type *)((u8 *)_stack + _top))
-#define POP_INT()                                                              \
-  TOP_AS_TYPE(int);                                                            \
-  _top--
+#define POP_INT(ref)                                                           \
+  _top--;                                                                      \
+  ref = TOP_AS_TYPE(int)
 
-#define POP_DOUBLE()                                                           \
-  TOP_AS_TYPE(double);                                                         \
-  _top--
+#define POP_DOUBLE(ref)                                                        \
+  _top--;                                                                      \
+  ref = TOP_AS_TYPE(double)
 
-#define POP_FLOAT()                                                            \
-  TOP_AS_TYPE(float);                                                          \
-  _top--
+#define POP_FLOAT(ref)                                                         \
+  _top--;                                                                      \
+  ref = TOP_AS_TYPE(float)
 
-#define POP_LONG()                                                             \
-  TOP_AS_TYPE(long);                                                           \
-  _top--
+#define POP_LONG(ref)                                                          \
+  _top--;                                                                      \
+  ref = TOP_AS_TYPE(long)
 
 #define STORE_TOP(type, value) (*(type *)((u8 *)_stack + _top) = (value))
 
@@ -41,259 +42,321 @@
   _top++
 
 #define POP_TWO(type, macro)                                                   \
-  type lhs = macro();                                                          \
-  type rhs = macro()
+  macro(type lhs);                                                             \
+  macro(type rhs)
 
 __attribute__((no_sanitize("address", "undefined", "leak"))) void
 ByteCodeInterpreter::Run()
 {
   for (; _pc < _code_size;)
-    switch (static_cast<OpCode>(_code_buf[_pc]))
+    switch (static_cast<OpCode>(*(u2 *)(_code_buf + _pc)))
     {
     case OpCode::kIAdd:
     {
       POP_TWO(int, POP_INT);
       PUSH_INT(lhs + rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kISub:
     {
       POP_TWO(int, POP_INT);
       PUSH_INT(lhs - rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kIMul:
     {
       POP_TWO(int, POP_INT);
       PUSH_INT(lhs * rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kIDiv:
     {
       POP_TWO(int, POP_INT);
       PUSH_INT(lhs / rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kIMod:
     {
       POP_TWO(int, POP_INT);
       PUSH_INT(lhs % rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kDAdd:
     {
       POP_TWO(double, POP_DOUBLE);
       PUSH_DOUBLE(lhs + rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kDSub:
     {
       POP_TWO(double, POP_DOUBLE);
       PUSH_DOUBLE(lhs - rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kDMul:
     {
       POP_TWO(double, POP_DOUBLE);
       PUSH_DOUBLE(lhs * rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kDDiv:
     {
       POP_TWO(double, POP_DOUBLE);
       PUSH_DOUBLE(lhs / rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kFAdd:
     {
       POP_TWO(float, POP_FLOAT);
       PUSH_FLOAT(lhs + rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kFSub:
     {
       POP_TWO(float, POP_FLOAT);
       PUSH_FLOAT(lhs - rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kFMul:
     {
       POP_TWO(float, POP_FLOAT);
       PUSH_FLOAT(lhs * rhs);
-      _pc++;
+      _pc += 2;
     }
     case OpCode::kFDiv:
     {
       POP_TWO(float, POP_FLOAT);
       PUSH_FLOAT(lhs / rhs);
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kIConst:
     {
-      int val = *(int *)(_code_buf + _pc + 1);
+      int val = *(int *)(_code_buf + _pc + 2);
       PUSH_INT(val);
-      _pc += 5;
+      _pc += 6;
       break;
     }
     case OpCode::kDConst:
     {
-      double val = *(double *)(_code_buf + _pc + 1);
+      double val = *(double *)(_code_buf + _pc + 2);
       PUSH_DOUBLE(val);
-      _pc += 9;
+      _pc += 10;
       break;
     }
     case OpCode::kFConst:
     {
-      float val = *(float *)(_code_buf + _pc + 1);
+      float val = *(float *)(_code_buf + _pc + 2);
       PUSH_FLOAT(val);
-      _pc += 5;
+      _pc += 6;
       break;
     }
     case OpCode::kLConst:
     {
-      long val = *(long *)(_code_buf + _pc + 1);
+      long val = *(long *)(_code_buf + _pc + 2);
       PUSH_LONG(val);
-      _pc += 9;
+      _pc += 10;
       break;
     }
     case OpCode::kICastD:
     {
-      int val = POP_INT();
+      int val;
+      POP_INT(val);
       PUSH_DOUBLE(static_cast<double>(val));
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kICastF:
     {
-      int val = POP_INT();
+      int val;
+      POP_INT(val);
       PUSH_FLOAT(static_cast<float>(val));
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kICastL:
     {
-      int val = POP_INT();
+      int val;
+      POP_INT(val);
       PUSH_LONG(static_cast<long>(val));
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kLCastD:
     {
-      long val = POP_LONG();
+      long val;
+      POP_LONG(val);
       PUSH_DOUBLE(static_cast<double>(val));
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kLCastF:
     {
-      long val = POP_LONG();
+      long val;
+      POP_LONG(val);
       PUSH_FLOAT(static_cast<float>(val));
-      _pc++;
+      _pc += 2;
       break;
     }
     case OpCode::kLCastI:
     {
-      long val = POP_LONG();
+      long val;
+      POP_LONG(val);
       PUSH_INT(static_cast<int>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kFCastI:
     {
-      float val = POP_FLOAT();
+      float val;
+      POP_FLOAT(val);
       PUSH_INT(static_cast<int>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kFCastL:
     {
-      float val = POP_FLOAT();
+      float val;
+      POP_FLOAT(val);
       PUSH_LONG(static_cast<long>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kFCastD:
     {
-      float val = POP_FLOAT();
+      float val;
+      POP_FLOAT(val);
       PUSH_DOUBLE(static_cast<double>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kDCastI:
     {
-      double val = POP_DOUBLE();
+      double val;
+      POP_DOUBLE(val);
       PUSH_INT(static_cast<int>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kDCastF:
     {
-      double val = POP_DOUBLE();
+      double val;
+      POP_DOUBLE(val);
       PUSH_FLOAT(static_cast<float>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kDCastL:
     {
-      double val = POP_DOUBLE();
+      double val;
+      POP_DOUBLE(val);
       PUSH_LONG(static_cast<long>(val));
-      _pc++;
+      _pc += 2;
+      break;
     }
     case OpCode::kLoadI:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      PUSH_INT(*(int *)(_stack + ix));
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      PUSH_INT(*(int *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kLoadD:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      PUSH_DOUBLE(*(double *)(_stack + ix));
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      PUSH_DOUBLE(*(double *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kLoadF:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      PUSH_FLOAT(*(float *)(_stack + ix));
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      PUSH_FLOAT(*(float *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kLoadL:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      PUSH_LONG(*(long *)(_stack + ix));
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      PUSH_LONG(*(long *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kStoreI:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      *(int *)(_stack + ix) = POP_INT();
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      POP_INT(*(int *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kStoreD:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      *(double *)(_stack + ix) = POP_DOUBLE();
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      POP_DOUBLE(*(double *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kStoreF:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      *(float *)(_stack + ix) = POP_FLOAT();
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      POP_FLOAT(*(float *)(_local_var_table + ix));
+      _pc += 4;
+      break;
     }
     case OpCode::kStoreL:
     {
-      int ix = *(u2 *)(_code_buf + _pc + 1);
-      *(long *)(_stack + ix) = POP_LONG();
-      _pc += 3;
+      int ix = *(u2 *)(_code_buf + _pc + 2);
+      POP_LONG(*(long *)(_local_var_table + ix));
+      _pc += 4;
+      break;
+    }
+    case OpCode::kPrintI:
+    {
+      int val;
+      POP_INT(val);
+      std::cout << val << "\n";
+      _pc += 4;
+      break;
+    }
+    case OpCode::kPrintF:
+    {
+      float val;
+      POP_FLOAT(val);
+      std::cout << val << "\n";
+      _pc += 4;
+      break;
+    }
+    case OpCode::kPrintD:
+    {
+      double val;
+      POP_DOUBLE(val);
+      std::cout << val << "\n";
+      _pc += 4;
+      break;
+    }
+    case OpCode::kPrintL:
+    {
+      long val;
+      POP_LONG(val);
+      std::cout << val << "\n";
+      _pc += 4;
+      break;
     }
     default:
-      panic("unimplemented bytecode", __FILE__, __LINE__);
+      char buf[ERROR_MSG_LEN];
+      std::snprintf(buf, ERROR_MSG_LEN, "unimplemented bytecode %d",
+                    _code_buf[_pc]);
+      panic(buf, __FILE__, __LINE__);
     }
 }
--- src/include/bytecodeinterpreter.h
+++ src/include/bytecodeinterpreter.h
@@ -2,6 +2,7 @@
 
 #pragma once
 #include "types.h"
+#include <cassert>
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -12,18 +13,24 @@ class ByteCodeInterpreter
 private:
   char *_code_buf;
   u8 *_stack;
+  u8 *_local_var_table;
+  int _var_size;
   int _stack_size;
   int _code_size;
   int _pc;
   int _top;
 
 public:
-  explicit ByteCodeInterpreter(const char *code, int len, int stack_size)
-      : _code_size(len), _pc(0), _stack_size(stack_size), _top(0)
+  explicit ByteCodeInterpreter(const char *code, int len, int stack_size,
+                               int var_size)
+      : _code_size(len), _pc(0), _stack_size(stack_size), _top(0),
+        _var_size(var_size)
   {
     _stack = (u8 *)malloc(stack_size);
     _code_buf = (char *)malloc(len);
     std::memcpy(_code_buf, code, len);
+    assert((var_size << 3) > 0);
+    _local_var_table = (u8 *)malloc(var_size << 3);
   }
 
   virtual ~ByteCodeInterpreter()
@@ -32,6 +39,8 @@ public:
       free(_stack);
     if (_code_buf != NULL)
       free(_code_buf);
+    if (_local_var_table != NULL)
+      free(_local_var_table);
   }
 
   __attribute__((no_sanitize("address", "undefined", "leak"))) void Run();
