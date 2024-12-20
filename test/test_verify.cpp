61a1d6da35af9e61da10fa9017d81c7bcbbe69fa
changlehung <changlehung@gmail.com>
2024-12-20 17:06:24 +0800

    feat: add ir verify tests

---
+++ test/test_verify.cpp
@@ -0,0 +1,29 @@
+
+#include "lexer.h"
+#include "loader.h"
+#include "parser.h"
+#include "types.h"
+#include "util.h"
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    panic("invalid argc", __FILE__, __LINE__);
+  }
+  const char *filename = argv[1];
+  CoreVMLexer lexer{FileInputStream(filename)};
+  const std::vector<CoreVMADT> &result = lexer.DoParse();
+  std::ostringstream os;
+  lexer.Serialize(os);
+  std::cout << os.str() << std::endl;
+  CVMType::InitIntrinsicType();
+  Parser parser{result};
+  auto ir = parser.Build();
+  ir->Verify();
+  return 0;
+}
